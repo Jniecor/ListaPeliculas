91c63bb48d4b35dc00f4a29787b334563b781726
Jniecor <jniecor515@g.educaand.es>
2022-04-06 10:15:52 +0200

    JNC. Revision 7. Modificacion del codigo

--- src/main/java/es/julionieto/listapeliculas/App.java
+++ src/main/java/es/julionieto/listapeliculas/App.java
@@ -6,8 +6,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -18,43 +20,50 @@ public class App extends Application {
     public void start(Stage stage) {
         
         VBox root = new VBox();
-        root.setAlignment(Pos.TOP_LEFT);
-        root.setSpacing(10);
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(50);
+        root.setStyle("-fx-background-image: url('/images/Fondo.jpg');");
         Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
+        stage.setTitle("Procesamiento XML");
         stage.show();
         HBox hRoot = new HBox();
         hRoot.setAlignment(Pos.CENTER);
-        hRoot.setSpacing(10);
+        hRoot.setSpacing(50);
+        HBox h2Root = new HBox();
+        h2Root.setAlignment(Pos.CENTER);
+        h2Root.setSpacing(50);
         
-//        TextArea textArea = new TextArea();
-//        root.getChildren().add(textArea);
-//        textArea.setEditable(false);
-        Label label = new Label();
-        root.getChildren().add(label);
+        Label labelTitulo = new Label();
+        labelTitulo.setFont((new Font("Liberation Serif", 22)));
+        labelTitulo.setStyle("-fx-font-weight: bold");
+        root.getChildren().add(labelTitulo);
+        Label labelXML = new Label();
+        labelXML.setFont((new Font("Liberation Serif", 18)));
         root.getChildren().add(hRoot);
+        root.getChildren().add(h2Root);
         
-        Pelicula pelicula = new Pelicula();
-        Pelicula pelicula0 = new Pelicula();
-        pelicula0.setTitulo("Los 7");
-        pelicula0.setDirector("Tarantino");
-        pelicula0.setActores("Will Smith, Jason Statham");
-        pelicula0.setDuracion(124);
-        pelicula0.setFechaEstreno("12-04-2023");
-        Pelicula pelicula1 = new Pelicula();
-        pelicula1.setTitulo("Los 8");
-        pelicula1.setDirector("Tarantino");
-        pelicula1.setActores("Will Smith");
-        pelicula1.setDuracion(125);
-        pelicula1.setFechaEstreno("12-04-2024");
+//        Pelicula pelicula = new Pelicula();
+//        Pelicula pelicula0 = new Pelicula();
+//        pelicula0.setTitulo("Los 7");
+//        pelicula0.setDirector("Tarantino");
+//        pelicula0.setActores("Will Smith, Jason Statham");
+//        pelicula0.setDuracion(124);
+//        pelicula0.setFechaEstreno("12-04-2023");
+//        Pelicula pelicula1 = new Pelicula();
+//        pelicula1.setTitulo("Los 8");
+//        pelicula1.setDirector("Tarantino");
+//        pelicula1.setActores("Will Smith");
+//        pelicula1.setDuracion(125);
+//        pelicula1.setFechaEstreno("12-04-2024");
         
         //Creamos la lista y añadimos las peliculas
         Peliculas peliculas = new Peliculas();
-        peliculas.getListaPeliculas().add(pelicula0);
-        peliculas.getListaPeliculas().add(pelicula1);
+//        peliculas.getListaPeliculas().add(pelicula0);
+//        peliculas.getListaPeliculas().add(pelicula1);
         
         final Button botonGuardar = new Button("Guardar Archivo");
-        hRoot.getChildren().add(botonGuardar);
+        h2Root.getChildren().add(botonGuardar);
         botonGuardar.setOnAction((t) -> {
             try {
                 UtilXML.guardarArchivoXML(stage, peliculas);
@@ -68,12 +77,13 @@ public class App extends Application {
             }
         });
         final Button botonImportar = new Button("Importar Archivo");
-        hRoot.getChildren().add(botonImportar);
+        h2Root.getChildren().add(botonImportar);
         botonImportar.setOnAction((t) -> {
             try {
                 Peliculas peliculasImport = UtilXML.importarArchivoXML(stage);
                 System.out.println("Numero de peliculas importadas: " + peliculasImport.getListaPeliculas().size());
                 peliculas.unirPeliculas(peliculasImport);
+                labelXML.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
             } catch(Exception ex){
                 ex.printStackTrace();
                 Alert alert = new Alert(Alert.AlertType.WARNING);
@@ -86,15 +96,15 @@ public class App extends Application {
         
         final Button botonAnterior = new Button("Anterior");
         hRoot.getChildren().add(botonAnterior);
+        hRoot.getChildren().add(labelXML);
         botonAnterior.setOnAction((t) -> {       
             peliculaActual--;
             if (peliculaActual < 0){
                 peliculaActual = 0;
             }
             try {
-//                textArea.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
-                label.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
-
+                labelXML.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
+                labelTitulo.setText("Procesamiento XML. Usted está en la "+(peliculaActual+1)+" posición.");
             } catch(Exception ex){
                 ex.printStackTrace();
                 Alert alert = new Alert(Alert.AlertType.WARNING);
@@ -113,8 +123,8 @@ public class App extends Application {
                 peliculaActual = peliculas.getListaPeliculas().size() -1 ;
             }
             try {
-//                textArea.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
-                label.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
+                labelXML.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
+                labelTitulo.setText("Procesamiento XML. Usted está en la "+(peliculaActual+1)+" posición.");
 
             } catch(Exception ex){
                 ex.printStackTrace();
@@ -126,8 +136,8 @@ public class App extends Application {
             }
         });
         
-//        textArea.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
-        label.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
+//        labelXML.setText(peliculas.getListaPeliculas().get(peliculaActual).toString());
+        labelTitulo.setText("Procesamiento XML. Usted está en la "+(peliculaActual+1)+" posición.");
         
     }
     
